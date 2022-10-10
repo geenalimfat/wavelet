d00a3d7b78ea75baf36ae6cb2a5140f595b4bc92
geenalimfat <glimfat@ucsd.edu>
2022-10-09 20:10:55 -0700

    Search Engine file

---
+++ SearchEngine.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    String phrase = "Racoons";
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Phrase: %d", phrase);
+        } else if (url.getPath().equals("/exclamation")) {
+            phrase = phrase + "!";
+            return String.format("Exalmation added!");
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/change")) {
+                String[] parameters = url.getQuery().split("=");
+     
+                if (parameters[0].equals("s")) {
+                    phrase = phrase + parameters[1]; 
+                //    num += Integer.parseInt(parameters[1]);
+                    return String.format("The phrase is now: %d", parameters[1], phrase);
+                }
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+    
+}
+
+
