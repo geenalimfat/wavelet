3e2c75dc9c758184b084ffbfcd3efeef2534ed53
geenalimfat <glimfat@ucsd.edu>
2022-10-20 15:48:19 -0700

    oops

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,31 +1,40 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
-    // various requests.
-    String phrase = "Racoons";
+    // various requests.    
+    ArrayList<String> list = new ArrayList<String>();
 
     public String handleRequest(URI url) {
+
+        String searched = "";
+
         if (url.getPath().equals("/")) {
-            return String.format("Phrase: %d", phrase);
-        } else if (url.getPath().equals("/exclamation")) {
-            phrase = phrase + "!";
-            return String.format("Exalmation added!");
-        } else {
-            System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/change")) {
-                String[] parameters = url.getQuery().split("=");
-     
-                if (parameters[0].equals("s")) {
-                    phrase = phrase + parameters[1]; 
-                //    num += Integer.parseInt(parameters[1]);
-                    return String.format("The phrase is now: %d", parameters[1], phrase);
-                }
+            return String.format("Search engine home");
+        } 
+        else if (url.getPath().equals("/add")) {
+            String[] parameters = url.getQuery().split("=");
+            if(parameters[0].equals("s")){
+                list.add(parameters[1]);
+                return "adding to history...";
             }
+        }
+        else if (url.getPath().equals("/search")) {
+            String[] parameter = url.getQuery().split("=");
+            if(parameter[0].equals("s")) {
+                for(String s : list){
+                    if(s.contains(parameter[1])){
+                        searched = searched + " " + s; 
+                    }
+                }
+                return searched;
+            }
+        } 
             return "404 Not Found!";
         }
-    }
+    
 }
 
 class SearchEngine {
@@ -39,7 +48,7 @@ class SearchEngine {
 
         Server.start(port, new Handler());
     }
-    
-}
+}    
+
 
 
